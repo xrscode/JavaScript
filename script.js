5aec66f379860ea891b01d0e1dec71037bac7a3f
xrscode <120644276+xrscode@users.noreply.github.com>
2023-02-13 12:53:20 +0000

    Objects and Arrays
    Pulling information out of an object.

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ const jonas = {
     job: 'Teacher',
     friends: ['Michael', 'Peter', 'Steven']
 } ;
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+
+const interestedIn = prompt('What do you want to know about Jonas?  Choose between firstName, lastName, age, job and friends.');
+
+
+if(jonas[interestedIn]) {
+    console.log(jonas[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends.') ;
+}

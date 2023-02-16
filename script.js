fca12d28ccc6f83a09ff8f09f09e7e8eb1c0ac06
xrscode <120644276+xrscode@users.noreply.github.com>
2023-02-16 15:15:35 +0000

    Coding Challenge 3 - Mark and John BMI
    Miniature coding challenge to work out who has the highest BMI.

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 'use strict'
 
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYear: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: true,
-    calcAge: function() {
-        this.age = 2037 - this.birthYear;
-        return this.age;
+//Mark Miller Object
+const markMiller = {
+    firstName: 'Mark',
+    secondName: 'Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = (this.mass)/(this.height**2);
+        return this.bmi;
+    },
+    markSummary: function() {
+        return `${this.firstName} ${this.secondName} has a BMI value of ${this.calcBMI()}.`;
     },
 
-    getSummary: function() {
-        return `${this.firstName} is a ${this.calcAge()} year old, ${this.job}.  He has ${this.hasDriversLicense ? 'a' : 'no'} drivers license.`
+};
+markMiller.calcBMI();
+console.log(markMiller.bmi);
 
-    }
+console.log(markMiller.markSummary());
+
+
+//John Smith Object
+const johnSmith = {
+    firstName: 'John',
+    secondName: 'Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = (this.mass)/(this.height**2);
+        return this.bmi;
+    },
+    johnSummary: function() {
+        return `${this.firstName} ${this.secondName} has a BMI value of ${this.calcBMI()}.`
+    },
+    
+};
+
+
+console.log(johnSmith.johnSummary());
+
+//if/else Higher BMI
+if (markMiller.bmi > johnSmith.bmi) {
+    console.log(`${markMiller.firstName} ${markMiller.secondName} has a BMI of ${markMiller.bmi}.  It is higher than Johns BMI which is only ${johnSmith.bmi}.`);
+} else if (markMiller.bmi < johnSmith.bmi) {
+    console.log(`${johnSmith.firstName} ${johnSmith.secondName} has a BMI of ${johnSmith.bmi}.  It is higher than Mark's BMI which is only ${markMiller.bmi}.`);
+} else if (markMiller.bmi === johnSmith.bmi) {
+    console.log('They both have the same BMI.');
 } ;
-
-console.log(jonas.getSummary());

acae570632d2c0a3c44e2fbbb523fd710a76ea6a
xrscode <120644276+xrscode@users.noreply.github.com>
2023-03-08 10:35:11 +0000

    Some Codewars Challenges

--- script.js
+++ script.js
@@ -1,198 +1,31 @@
-'use strict'
+"use strict";
+// let n = 5;
+// let m = 6;
 
-//for loop keeps running while condition is TRUE.
-// for (let rep = 1; rep <= 20; rep++) {
-//     console.log(`Print this statement multiple times. Repetition number: ${rep}.`) ;
-// } ; 
-
-
-
-// //Array
-// const jonas = [
-//     'Jonas',                       //Position 0
-//     'Schmedtmann',                 //Position 1
-//     2037 - 1991,                   //Position 2
-//     'teacher',                    // Position 3
-//     ['Michael', 'Peter', 'Steven'] //Position 4
-// ] ;
-
-// // for (let i = jonas.length - 1; i >= 0; i-- ){
-// //     console.log(i, jonas[i]);
-// // } ;
-
-// for (let exercise = 1; exercise <= 3; exercise++){
-//     console.log(`Starting Exercise ${exercise}.`) ;
-
-//     for (let rep = 1; rep < 6; rep++) {
-//         console.log(`Lifting weight rep number: ${rep}.`) ;
-//     }
+// let paperWork = function () {
+//   let result = n * m;
+//   if (result >= 0) {
+//     return result;
+//   } else if (result < 0) {
+//     return "Invalid number.";
+//   }
 // };
 
+// console.log(paperWork());
 
+//Function to Return String times Integer.
+//n is integer.
+//s is string.
+// function repeatStr(n, s) {
+//   return s.repeat(n);
+// }
 
+//Function to Split a string and convert it into an array of words.
+// function stringToArray(string) {
+//   // code code code
+//   return string.split(" ");
+// }
 
-
-
-
-
-
-
-// // for (let i = 0; i < jonas.length ; i++) {
-// //     if(typeof jonas[i] === 'number') break;
-    
-// //     console.log(jonas[i], typeof jonas[i])
-// // } ;
-    
-
-
-// //     //types[i] = typeof jonas[i];
-// //     types.push(typeof jonas[i]);
-// // };
-
-// // console.log(types);
-
-// // const years = [1991, 2007, 1969, 2020];
-// // const ages = [];
-
-// // for (let i = 0; i < years.length; i++ ) {
-// //     ages.push(2023 - years[i]);
-// // };
-
-// // console.log(ages);
-
-// // // Continue & Break Statements
-
-// 
-
-// const jonas = [
-//     'Jonas',
-//     'Schmedtmann',
-//     2037-1991,
-//     'Teacher',
-//     ['Michael', 'Peter', 'Steven'],
-//     true,
-// ];
-
-// for (let i = jonas.length -1; i >= 0; i--) {
-//     console.log(i, jonas[i]);
-// };
-
-// for (let exercise = 1; exercise <= 3; exercise++) {
-//     console.log(``);
-
-//     for (let rep = 10; rep >= 0; rep--){
-    
-//     };
-// } ;
-
-// let rep = 1;
-// while (rep <= 10) {
-//     console.log(`WHILE: Lifting weights repetition ${rep}`);
-//     rep++;
-// };
-
-// let dice = Math.trunc(Math.random() * 6) + 1;
-    
-// while (dice !== 6) {
-//     console.log(`You rolled a ${dice}.`) ;
-//     dice = Math.trunc(Math.random() * 6) + 1;
-//     if (dice === 6) console.log(`Loop is about to end.`);
-// } ;
-
-//JavaScript Fundamentals Coding Challenge #4
-
-//Array for Bills containing test values
-// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52] ;
-// console.log(bills);
-
-// const tips = [] ;
-// const totals = [] ;
-
-
-// const tip = function (a) {
-//     return (a * 0.2) ;
-// } ;
-
-// let billTotal = function (a) {
-//     return a + tip(a);
-// } ;
-
-// let billFinal = function (a) {
-//     return tip(a) + a;
-// } ;
-
-// console.log(billFinal(22)) ; 
-
-
-// //for Loop to add to our array
-// for (let i = 0; i < bills.length; i++) {
-//     tips.push(tip(bills[i])) ;
-// } ; 
-
-// //for Loop to add one array to the other
-// for (let i = 0; i < bills.length; i++) {
-//     totals.push(bills[i] + tips[i]) ;
-// } ; 
-
-// console.log(`This array is for tips; ${tips}`) ; 
-// console.log(`This array is for totals; ${totals}`); 
-
-// let arr = [
-//     5,
-//     6,
-//     7,
-//     9, 
-// ] ; 
-
-
-// const calcAverage = function(a) {
-//     let sum = 0;
-//     for (let i = 0; i < a.length; i++) {
-//         sum = sum + a[i] ;
-//     } ; 
-    
-//     return sum / a.length;
-// } ;
-
-// console.log(calcAverage(arr)); 
-// console.log(calcAverage(totals));
-
-
-
-
-
-
-// for (let i = 0; i < bills.length ; i++ ) {
-//     console.log(`Your bill total is $${bills[i]}.  Your tip total is $${tip(bills[i])}.  Your bill total is: $${billTotal(bills[i])}.`) ;
-// } ;
-
-// let ham = [
-//     'Poop',
-//     'Walk',
-//     'Growls'
-// ];
-
-// console.log(ham);
-
-// console.log(ham[1]);
-
-let input = 'Why hello there.  My name is Dylan and I am a cheeky dog.';
-const vowels = ['a', 'e', 'i', 'o', 'u'];
-let resultArray = [];
-
-//Loop to iterate through each letter of the input variable text.
-for (let i = 0; i < input.length; i++) {
-    if (input[i] === 'e' || input[i] === 'u') {
-        resultArray.push(input[i]);
-    };
-    
-    for (let b = 0; b < vowels.length; b++) {
-        if (vowels[b] === input[i]) {
-            resultArray.push(vowels[b]);
-        };
-    };
-};
-
-let resultString = resultArray.join('').toUpperCase();
-
-console.log(resultString);
+// console.log(stringToArray("Hello I am Dylan"));
+//Given an array of integers, return a new array with each value doubled.
+// function maps(x) {}
